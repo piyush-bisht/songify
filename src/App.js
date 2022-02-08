70d6867e6d455b9887e5f780e5cfa10c2ba0f909
ankupainuly <ankurpainuly010@gmail.com>
2022-02-08 16:45:14 +0530

    adding like and unlike features on tracks.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import { Component } from "react";
-
+//-----------------------------------------------------------------
 import ReactLoading from 'react-loading';
-import { Route,
-          BrowserRouter as Router,
-          Switch
-} from "react-router-dom";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+//-----------------------------------------------------------------
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { PrivateRoute, PublicRoute } from './components/Routes';
+//-----------------------------------------------------------------
 import Main from './pages/Main';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
-
-import { auth } from './services/firebase';
-
-import { PrivateRoute, PublicRoute } from './components/Routes';
-
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import "./App.css";
-import AudioPlayer from "./pages/AudioPlayer";
 import Tracks from "./pages/Tracks";
-import Cookies from 'universal-cookie';
+import Signup from './pages/Signup';
+import AudioPlayer from "./pages/AudioPlayer";
+//-----------------------------------------------------------------
+import { auth, db } from './services/firebase';
+//-----------------------------------------------------------------
+
 class App extends Component
 {
   constructor() {
@@ -28,30 +24,27 @@ class App extends Component
 
     this.state = {
       authenticated: false,
+      spotifyAuth: false,
       loading: true,
     };
   }
-  
+
   componentDidMount() {
-    const cookies= new Cookies();
-    var newPlayerState={
-    
-      nowPlaying:"",
-      playingSongImage:"",
-      playingSongLink:"",
-      playingArtist:"",
-      isPlaying:false,
-      currentTime:0,
-      duration:0
-    }
-    cookies.set("playerState",newPlayerState,{ path: '/' });
-    
     auth().onAuthStateChanged((user) =>{
         if (user) {
             this.setState({
               authenticated: true,
               loading: false,
             })
+            try {
+              console.log(auth().currentUser)
+                db.ref("users").child(auth().currentUser.uid).child({
+                  email: auth().currentUser.email,
+                })
+            }
+            catch(error) {
+                console.log(error.message);
+            }
         } else {
             this.setState({
               authenticated: false,
@@ -62,34 +55,23 @@ class App extends Component
   }
 
   render() {
-    
+    console.log(this.state.spotifyAuth)
     return this.state.loading ? (
         <div className="loading-indicator">
             <ReactLoading type="spin" color="blue" height={'3%'} width={'3%'}/>
         </div>
       ) : (
-       <div>
-      <Router>
-        
-        <Switch>
-        
-          <PublicRoute exact path="/" authenticated={this.state.authenticated} component={Home} />
-          <PublicRoute path="/signup" authenticated={this.state.authenticated} component={Signup} />
-          <PublicRoute path="/login" authenticated={this.state.authenticated} component={Login} />
-          
-          <PrivateRoute path="/main" authenticated={this.state.authenticated} component={Main} />
-          <PrivateRoute path="/tracks" authenticated={this.state.authenticated} component={Tracks} />
-        
-          
-          
-          
-          
-        </Switch>
-        
-      </Router>
-      </div> 
-    );
-    
+              <Router>
+                <Switch>
+                  <PublicRoute exact path="/" authenticated={this.state.authenticated} component={Home} />
+                  <PublicRoute path="/login" authenticated={this.state.authenticated} component={Login} />
+                  <PublicRoute path="/signup" authenticated={this.state.authenticated} component={Signup} />
+                  <PrivateRoute path="/main" authenticated={this.state.authenticated} component={Main} />
+                  <PrivateRoute path="/player" authenticated={this.state.authenticated} component={AudioPlayer} />
+                  <PrivateRoute path="/tracks" authenticated={this.state.authenticated} component={Tracks} />
+                </Switch>
+              </Router>
+          )
   }
 }
 export default App;
