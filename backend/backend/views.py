acfd9944d5a939e62c2941174f9a01e4fbd1620a
Piyush <piyush.bisht3@gmail.com>
2022-02-09 17:39:48 +0530

    deleted configuration variable

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -9,16 +9,6 @@ from firebase_admin import db
 import pandas as pd
 
 from recSystem import getRecommendations
-config ={
-    "apiKey": "AIzaSyDDbmo5GRLP3H8TTJcFiTUCHkgTDH9qHMI",
-    "authDomain": "songify-9e677.firebaseapp.com",
-    "databaseURL": "https://songify-9e677-default-rtdb.firebaseio.com",
-    "projectId": "songify-9e677",
-    "storageBucket": "songify-9e677.appspot.com",
-    "messagingSenderId": "1046063845211",
-    "appId": "1:1046063845211:web:2360ca2e9f63ec3ffbd47e",
-    "measurementId": "G-S0YYR7F2WZ"
-}
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate('./secret.json')
 # Initialize the app with a service account, granting admin privileges
@@ -29,14 +19,12 @@ ref = db.reference('/users')
 docs=ref.get()
 for key,value in docs.items():
     print(key)
-    # print(value['likedSongs'])
     print("/n")
 
 @csrf_exempt
 def index(request):
     if request.method == "GET":
         print("GET REQ INIT")
-        # ans=[]
         
         return HttpResponse(docs)
     elif request.method == "POST":
